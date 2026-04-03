89655aa66a1c1c83b0704bc372fd97124fbb232d
Abhikrant <abhikrant.dhepe@gmail.com>
2026-04-03 22:58:32 +0530

    Final UI changes and Redis implementation

--- scalable-chat-app/apps/server/src/services/socket.ts
+++ scalable-chat-app/apps/server/src/services/socket.ts
@@ -1,4 +1,21 @@
 import { Server } from "socket.io";
+import Redis from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
+
+const pub = new Redis({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+});
+
+const sub = new Redis({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+});
 
 class SocketService {
   private _io: Server;
@@ -10,6 +27,7 @@ class SocketService {
         origin: "*",
       },
     });
+    sub.subscribe("MESSAGES");
   }
 
   public initListeners() {
@@ -19,7 +37,16 @@ class SocketService {
       console.log("New Socket Connected ", socket.id);
       socket.on("event:message", async ({ message }: { message: string }) => {
         console.log("New Message Received", message);
+
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
       });
+    });
+
+    sub.on("message", (channel, message) => {
+      if (channel === "MESSAGES") {
+        console.log("New MEssage Rec from Server", message);
+        io.emit("message", message);
+      }
     });
   }
 
